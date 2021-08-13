e11dd4f53b8bb65555c4a82a6d7b7140b877f1c0
Nelton Fernandes <47947664+neltonf@users.noreply.github.com>
2021-08-13 15:47:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    var ham = document.getElementById("mobile");
-var menu = document.querySelector(".menu");
-var mobile = document.querySelector(".mobile")
-ham.addEventListener("click", toggleMenu);
+window.addEventListener("DOMContentLoaded", (event) => {
+  var ham = document.getElementById("mobile");
+  var menu = document.querySelector(".menu");
+  var mobile = document.querySelector(".mobile");
+  ham.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    mobile.style.transform = "rotate(90deg)";
-  } else {
-    menu.classList.add("showMenu");
-    xIcon.style.display = "block";
-    menuIcon.style.display = "none";
+  function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      mobile.style.transform = "rotate(0deg)";
+    } else {
+      menu.classList.add("showMenu");
+      mobile.style.transform = "rotate(90deg)";
+    }
   }
-}
+});
