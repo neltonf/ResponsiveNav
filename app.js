25be5f0e2cb0f4ad3a66a3dd51fc88010858edb0
Nelton Fernandes <47947664+neltonf@users.noreply.github.com>
2021-08-13 15:40:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ ham.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    mobile.style.transform = "rotate(90deg)";
+    mobile.style.transform = "rotate(0deg)";
   } else {
     menu.classList.add("showMenu");
-    mobile.style.transform = "rotate(0deg)";
+    mobile.style.transform = "rotate(90deg)";
   }
 }
 
